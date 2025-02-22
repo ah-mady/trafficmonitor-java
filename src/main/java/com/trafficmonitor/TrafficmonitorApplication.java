979f694d669eb89a3cd6dbddd815a5cee3b10d48
Ahmed <ahmad_bhai28@yahoo.com>
2025-02-22 19:07:39 +0100

    initialized the project

---
+++ src/main/java/com/trafficmonitor/TrafficmonitorApplication.java
@@ -0,0 +1,13 @@
+package com.trafficmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrafficmonitorApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TrafficmonitorApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/trafficmonitor/TrafficmonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trafficmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficmonitorApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
